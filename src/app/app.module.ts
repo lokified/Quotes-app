4114f3cb81f8f083d671a99e26f215a70da48245
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-09 23:13:43 +0300

    add fontawesome module and icons on the quote template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { QuoteDetailsComponent } from './quote-details/quote-details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Quote } from '../quote';
+import {  faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-quote-details',
@@ -9,6 +10,10 @@ import { Quote } from '../quote';
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote! : Quote;
+
+  faDown = faThumbsDown;
+  faUp = faThumbsUp;
+
   constructor() { }
 
   ngOnInit(): void {
