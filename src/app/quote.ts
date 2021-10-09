7966195b715a62a7d8a115322b828ae93620856f
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-09 22:41:22 +0300

    add a quote component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,9 @@
 export class Quote {
+    constructor (
+        public  yourName : string,
+        public  yourQuote : string,
+        public  authorName : string
+    ) {
+        
+    }
 }
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes : Quote [] = [
+    new Quote ('Sheldon Okware','Worth the time that you ought to keep','Loki'),
+    new Quote('Elon Musk','The first step is to establish that something is possible; then the probability will occur','Tesla'),
+ ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
